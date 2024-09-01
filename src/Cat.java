12f70a9964faada3da0afec4d379db79d9c366d8
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-09-01 23:49:10 +0300

    add a Dog class

--- src/Cat.java
+++ src/Cat.java
@@ -1,2 +1,19 @@
-public class Cat {
+public class Cat extends  Animal{
+    private  String litterPreference;
+
+    public  Cat(String name){
+        this.name = name;
+    }
+
+    public  Cat(){
+
+    }
+    public  String getLitterPreference(){
+        return  litterPreference;
+    }
+
+    public void setLitterPreference(String litterPreference){
+
+    }
+
 }
